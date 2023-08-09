7a07195629ca56f1f23f8eceb4873e6e0a170fb5
Mayank-Patel1015 <141281241+Mayank-Patel1015@users.noreply.github.com>
2023-08-09 17:23:47 -0400

    Update guess_number.py
    Change code for easier comprehension.

--- guess_number.py
+++ guess_number.py
@@ -1,17 +1,17 @@
 import random
 
-def guess_number(x):
-    random_number = random.randint(1,x)
+def guess_number(largest_number):
+    random_number = random.randint(1,largest_number)
     guess = 0
     guess = int(input("What number would you like to guess?: "))
     while random_number != guess:
         if(guess < random_number):
             print("Guess a higher number")
-        elif(guess > random_number):
+        if(guess > random_number):
             print("Guess a lower number")
         print("Guess again")
         guess = int(input("What number would you like to guess?: "))
     print(f"Congrats!! You guessed the random number ({str(guess)}) correctly.")
 
-x = int(input("What is the largest number you would like to guess?"))
-guess_number(x)
+largest_number = int(input("What is the largest number you would like to guess?"))
+guess_number(largest_number)
